85ed83b8968068df11eb07eb458307a428167c43
guiofranca <dankan282@gmail.com>
2023-05-21 19:04:21 -0300

    Fix

--- supabase/feck/20230518105232_link_medidores_grandezas.sql
+++
@@ -1,8 +0,0 @@
-create table
-  public.medidores_grandezas (
-    medidor_id bigint not null,
-    grandeza_id bigint not null,
-    constraint medidores_grandezas_pkey primary key (medidor_id, grandeza_id),
-    constraint medidores_grandezas_grandeza_id_fkey foreign key (grandeza_id) references grandezas (id) on delete cascade,
-    constraint medidores_grandezas_medidor_id_fkey foreign key (medidor_id) references medidores (id) on delete cascade
-  ) tablespace pg_default;
